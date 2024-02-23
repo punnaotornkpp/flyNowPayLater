a92ef1d0f5b284813202c473c3c8a96ddf05017a
benze0kg15930 <131743670+benze0kg15930@users.noreply.github.com>
2024-02-23 17:17:03 +0700

    custom-header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  dropdownVisible: boolean = true;
+
+  toggleDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
+  }
+
+}
+
