37b09fc463f939221a3ba1e71931b54a83c17c47
Nokfly2024! <Punnatorn.Yim@nokair.co.th>
2024-02-29 13:55:51 +0700

    fix all page for support responsive

--- src/app/shared/step/step.component.ts
+++ src/app/shared/step/step.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-step',
   templateUrl: './step.component.html',
-  styleUrl: './step.component.scss'
+  styleUrl: './step.component.scss',
 })
 export class StepComponent {
+  isSmallScreen: boolean = false;
 
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .subscribe((result) => {
+        this.isSmallScreen = result.matches;
+      });
+  }
 }
