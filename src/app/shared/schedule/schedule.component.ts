bcd8aff7940c9b9bfc7ce3ecba8bab3453384875
Nokfly2024! <Punnatorn.Yim@nokair.co.th>
2024-03-08 14:30:39 +0700

    add pipe decimal and fix schedule next and back step

--- src/app/shared/schedule/schedule.component.ts
+++ src/app/shared/schedule/schedule.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatTabChangeEvent } from '@angular/material/tabs';
 import { SubscriptionDestroyer } from '../../core/helper/subscriptionDestroyer.helper';
 import { IJourney } from '../../model/flight-schedule';
+import { SharedService } from '../../service/shared.service';
+import { SessionStorage } from '../../core/helper/session.helper';
 
 @Component({
   selector: 'app-schedule',
@@ -10,22 +12,52 @@ import { IJourney } from '../../model/flight-schedule';
 })
 export class ScheduleComponent extends SubscriptionDestroyer implements OnInit {
   @Input() value!: IJourney[];
+  @Input() currentDate: string = '';
   @Output() onNextClick = new EventEmitter<any>();
+  @Output() onBackClick = new EventEmitter<any>();
+  empty = false;
+  defaultTabIndex: number = 4;
+  totalIndex: number = 9;
 
-  constructor() {
+  constructor(
+    private sharedService: SharedService,
+    private session: SessionStorage
+  ) {
     super();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.value.length < 7) {
+      this.empty = true;
+    }
+  }
 
-  defaultTabIndex: number = 4;
+  onTabChange(event: MatTabChangeEvent, value: IJourney[]): void {
+    if (typeof window !== 'undefined' && window.sessionStorage) {
+      const sessionvalue = JSON.parse(this.session.get('history'));
+      // sessionvalue.form.journey[0].departureDate =
+      //   value[event.index].departureDate;
+      // console.log(value[event.index]);
+      // console.log(sessionvalue.form.journeys[0].departureDate);
+    }
 
-  onTabChange(event: MatTabChangeEvent): void {
-    const totalTabs = this.value.length + 2;
     if (event.index === 0) {
+      this.onBackClick.emit((resp: boolean) => {
+        this.empty = resp;
+        this.defaultTabIndex = 1;
+      });
+    } else if (event.index === this.totalIndex - 1) {
       this.onNextClick.emit();
-    } else if (event.index === totalTabs - 1) {
-      this.onNextClick.emit();
+    }
+    this.sharedService.triggerHeaderRefresh();
+  }
+
+  centerIndex() {
+    this.totalIndex = this.value.length + 2;
+    if (this.totalIndex % 2 === 0) {
+      this.defaultTabIndex = this.totalIndex / 2 - 1;
+    } else {
+      this.defaultTabIndex = Math.floor(this.totalIndex / 2);
     }
   }
 }
